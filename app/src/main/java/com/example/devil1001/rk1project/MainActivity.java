44aa29afdcdf12dc6a0fb97da670c65bf62bb7f1
devil1001 <jenek-devil@mail.ru>
2017-03-07 20:53:08 +0300

    fon

--- app/src/main/java/com/example/devil1001/rk1project/MainActivity.java
+++ app/src/main/java/com/example/devil1001/rk1project/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.devil1001.rk1project;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import ru.mail.weather.lib.News;
@@ -64,9 +67,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 boolean updateEnable = Storage.getInstance(MainActivity.this).loadIsUpdateInBg();
                 Storage.getInstance(MainActivity.this).saveIsUpdateInBg(!updateEnable);
-                if (updateEnable) {
+                if (!updateEnable) {
+                    Intent myIntent = new Intent(MainActivity.this, NewsService.class);
+                    myIntent.setAction(NewsService.NEWS_LOAD_ACTION);
+                    PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this,  0, myIntent, 0);
+                    AlarmManager alarmManager = (AlarmManager)MainActivity.this.getSystemService(Context.ALARM_SERVICE);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.add(Calendar.SECOND, 60);
+                    long frequency= 60 * 1000;
+                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), frequency, pendingIntent);
                     Toast.makeText(MainActivity.this, "Started", Toast.LENGTH_SHORT).show();
                 } else {
+                    Intent myIntent = new Intent(MainActivity.this, NewsService.class);
+                    myIntent.setAction(NewsService.NEWS_LOAD_ACTION);
+                    PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this,  0, myIntent, 0);
+                    AlarmManager alarmManager = (AlarmManager)MainActivity.this.getSystemService(Context.ALARM_SERVICE);
+                    alarmManager.cancel(pendingIntent);
                     Toast.makeText(MainActivity.this, "Stopped", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -78,17 +95,17 @@ public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(final Context context, final Intent intent) {
-                if (intent.getAction().equals(NewsService.NEWS_CHANGED_ACTION)) {
-                    News news = Storage.getInstance(MainActivity.this).getLastSavedNews();
-                    headerNews.setText(news.getTitle());
-                    dateNews.setText(DateFormat.format("MM/dd/yyyy", new Date(news.getDate())).toString());
-                    bodyNews.setText(news.getBody());
-                }
-                if (intent.getAction().equals(NewsService.NEWS_ERROR)) {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
+            if (intent.getAction().equals(NewsService.NEWS_CHANGED_ACTION)) {
+                News news = Storage.getInstance(MainActivity.this).getLastSavedNews();
+                headerNews.setText(news.getTitle());
+                dateNews.setText(DateFormat.format("MM/dd/yyyy HH:mm:ss", new Date(news.getDate())).toString());
+                bodyNews.setText(news.getBody());
             }
-        };
+            if (intent.getAction().equals(NewsService.NEWS_ERROR)) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onStart() {
@@ -103,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         News news = Storage.getInstance(MainActivity.this).getLastSavedNews();
         if (news != null) {
             headerNews.setText(news.getTitle());
-            dateNews.setText(DateFormat.format("MM/dd/yyyy", new Date(news.getDate())).toString());
+            dateNews.setText(DateFormat.format("MM/dd/yyyy HH:mm:ss", new Date(news.getDate())).toString());
             bodyNews.setText(news.getBody());
         }
     }
